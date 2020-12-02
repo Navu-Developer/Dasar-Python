bce9de2f587e0ada696d5afe43c2300f2fe85961
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-02 12:56:25 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/03 Python Tipe Data/main.py
@@ -0,0 +1,13 @@
+# tipe data
+'''
+angka = integer / int (tanpa titik {Dalam python titik adalah koma})
+teks = string / str (penulisan variabel dengan petik {contoh: 'string'})
+desimal = float / float (sama seperti angka hanya dengan titik {contoh: 10.5})
+cek = boolean / bool (bernilai True atau False)
+sekuensial = list, tuple (data berurutan)  
+'''
+
+# data integer / int
+a = 1000
+print("Nilai a : ",a)
+print("variabel a bertipe data : ",type(a))
