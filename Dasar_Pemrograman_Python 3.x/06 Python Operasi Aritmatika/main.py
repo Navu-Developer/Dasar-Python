9846fbe172e0b4e16524788719b939bf042982c3
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-02 16:55:05 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/06 Python Operasi Aritmatika/main.py
@@ -0,0 +1,29 @@
+# Operasi aritmatika dalam Python
+# jumlah, kurang, kali, bagi, sisa bagi, pangkat
+
+a = int(input("Nilai a : "))
+b = int(input("Nilai b : "))
+
+# penjumlahan
+hasil = a + b
+print("a + b = ",hasil)
+
+# pengurangan
+hasil = a - b
+print("a - b = ",hasil)
+
+# Perkalian
+hasil = a * b
+print("a x b = ",hasil)
+
+# Pembagian
+hasil = a / b
+print("a : b = ",hasil) # hasil berupa float
+
+# Hasil bagi
+hasil1 = a // b
+print("a : b = ",hasil,"Bersisa : ",hasil1)
+
+# Pemangkatan
+hasil = a ** b
+print("a ^ b = ",hasil)
