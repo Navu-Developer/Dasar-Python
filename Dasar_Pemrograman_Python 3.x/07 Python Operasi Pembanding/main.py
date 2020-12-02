25b9fab13a42a8caf614dc8232a1fed5a220a194
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-02 17:06:28 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/07 Python Operasi Pembanding/main.py
@@ -0,0 +1,21 @@
+# Operasi pembanding dalam Python
+# Lebih besar / lebih kecil, sama dengan, tidak sama dengan
+
+a = 5
+b = 10
+
+# Lebih besar > lebih kecil < 
+c = a > b
+print(a,">",b,"=",c)
+c = b > a
+print(b,">",a,"=",c)
+c = a > 7
+print(a,"> 7 =",c)
+c = b > 7
+print(b,"> 7 =",c)
+
+# sama dengan ==
+c = a == b
+print(a,"==",b,"=",c)
+c = a == 5
+print(a,"== 5 =",c)
