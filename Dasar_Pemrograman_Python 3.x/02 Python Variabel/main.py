d2b5957a23d091b19820ca266e20a42fcdc67de3
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-02 12:42:20 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/02 Python Variabel/main.py
@@ -0,0 +1,20 @@
+# variabel a dan b
+a = 1 # variabel a menyimpan data integer bernilai 1
+b = 2 # variabel a menyimpan data integer bernilai 2
+
+# memanggil fariabel dengan fungsi print
+print("Nilai a : ",a)
+print("Nilai b : ",b)
+
+'''
+Python adalah interpreted language
+code dibaca per baris sesuai algoritma program
+'''
+
+a = 2
+print("Nilai a : ",a)
+
+c = a
+print("Nilai c = a : ",c)
+b = c
+print("Nilai b = c : ",b)
