ebef754ed14abff7b16d15fccd2e6928c4b7a11a
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-02 17:29:41 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/08 Python Operasi Penugasan/main.py
@@ -0,0 +1,26 @@
+# Operasi Penugasan pada Python
+# = , += , -= , *= , /= , ** =
+
+# sama dengan
+a = 18
+print("variabel a telah ditugaskan untuk menyimpan nilai 18")
+
+# tambah dengan
+a+=2
+print("variabel a telah ditugaskan untuk menambah nilainya dengan nilai 2 manjadi ",a)
+
+# kurang dengan
+a-=3
+print("variabel a telah ditugaskan untuk mengurangi nilainya dengan nilai 3 menjadi ",a)
+
+# kali dengan
+a*=2
+print("variabel a telah ditugaskan untuk mengalikan nilainya dengan nilai 2 menjadi ",a)
+
+# bagi dengan
+a/=5
+print("variabel a telah ditugaskan untuk membagikan nilainya dengan nilai 5 menjadi ",a)
+
+# pangkatkan dengan
+a**=2
+print("variabel a telah ditugaskan untuk memangkatkan nilainya dengan nilai 2 menjadi ",a)
