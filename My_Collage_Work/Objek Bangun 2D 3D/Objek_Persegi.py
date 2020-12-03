54d87aa790793125dd35d8b3e76f2b29ddc80dc0
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-03 15:15:38 +0700

    Create Objek_Persegi.py

---
+++ My_Collage_Work/Objek Bangun 2D 3D/Objek_Persegi.py
@@ -0,0 +1,10 @@
+class persegi(object):
+
+    def __init__(self, s=int(input())):
+        self.sisi = s
+
+    def keliling(self):
+        return(self.sisi*4)
+
+    def luas(self):
+        return(self.sisi**2)
