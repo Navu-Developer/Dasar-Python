e73e7eb6700ebea80b71b0d36e172b4c96faeb65
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-03 15:46:46 +0700

    Create main.py

---
+++ My_Collage_Work/Program Stack/Stack_Internal/main.py
@@ -0,0 +1,25 @@
+def main():
+    keep_alive = 0
+    item = []
+    while keep_alive == 0:    
+        print("="*25)
+        print("PROGRAM STACK INTERNAL")
+        print("Code\t: Python3")
+        print("="*25)
+        for i in item:
+            print(i, end=' ')
+            print(end=',')
+        print("panjang data : ",len(item))
+        print('\n')
+        print("1. tambah data")
+        print("2. hapus data")
+        print("0. exit")
+        choose = str(input("> "))
+        if choose == "1":
+            item.append(str(input()))
+        if choose == "2":
+            item.pop()
+        if choose == "0":
+            keep_alive+=1
+if __name__ == "__main__":
+    main()
