2b4d20005f2877db71c6135817e77a79df808092
nOtavaliduser <73369743+n0tavaliduser@users.noreply.github.com>
2020-12-26 21:28:19 +0700

    Create main.py

---
+++ Dasar_Pemrograman_Python 3.x/14 Python Abstract Data Type/main.py
@@ -0,0 +1,32 @@
+bulan = ['januari', 'februari', 'Maret', "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+
+tanggal = int(input("tanggal : "))
+bulan1 = int(input("bulan : "))
+tahun = int(input("tahun : "))
+kabisat = tahun % 4
+hari = 0
+
+def _bulan():
+    return(bulan[bulan1-1])
+
+if _bulan() == 'februari':
+    if kabisat == 0:
+        hari = 29
+    else:
+        hari = 28
+if _bulan() != 'februari':
+    if (bulan1%2)!=0:
+        hari = 31
+    else:
+        hari = 30
+
+
+print("tanggal %d/%s/%d"%(tanggal, _bulan(), tahun))
+if tanggal > hari+1:
+    print("tanggal tidak valid")
+elif tanggal < hari+1:
+    print("tanggal valid")
+    if kabisat == 0:
+        print("ini tahun kabisat")
+    elif kabisat != 0:
+        print("ini bukan tahun kabisat")
